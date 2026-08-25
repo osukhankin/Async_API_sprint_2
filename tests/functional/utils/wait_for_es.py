ee5257c5b7173f4afe5b78a0c5dc84535e4c418b
o.sukhankin <o.sukhankin@vk.team>
2026-08-25 23:33:51 +0300

    tests infra

---
+++ tests/functional/utils/wait_for_es.py
@@ -0,0 +1,17 @@
+import os
+import time
+
+from elasticsearch import Elasticsearch
+
+if __name__ == '__main__':
+    host = os.getenv('ELASTIC_HOST', 'elasticsearch')
+    port = os.getenv('ELASTIC_PORT', '9200')
+    schema = os.getenv('ELASTIC_SCHEMA', 'http://')
+    es_client = Elasticsearch(hosts=f'{schema}{host}:{port}')
+    while True:
+        try:
+            if es_client.ping():
+                break
+        except Exception:
+            pass
+        time.sleep(1)
---
+++ tests/functional/utils/wait_for_redis.py
@@ -0,0 +1,16 @@
+import os
+import time
+
+from redis import Redis
+
+if __name__ == '__main__':
+    host = os.getenv('REDIS_HOST', 'redis')
+    port = int(os.getenv('REDIS_PORT', '6379'))
+    redis_client = Redis(host=host, port=port)
+    while True:
+        try:
+            if redis_client.ping():
+                break
+        except Exception:
+            pass
+        time.sleep(1)
